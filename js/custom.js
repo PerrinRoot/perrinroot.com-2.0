b3a5e1286cc3da58ea3f966bf4053778511ede88
Perrin Root <122834235+PerrinRoot@users.noreply.github.com>
2024-03-01 19:29:02 -0500

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,68 @@
+
+
+  /*-------------------------------------------------------------------------------
+    PRE LOADER
+  -------------------------------------------------------------------------------*/
+
+  $(window).load(function(){
+    $('.preloader').fadeOut(1000); // set duration in brackets    
+  });
+
+
+  /*-------------------------------------------------------------------------------
+    jQuery Parallax
+  -------------------------------------------------------------------------------*/
+
+    function initParallax() {
+    $('#home').parallax("50%", 0.3);
+
+  }
+  initParallax();
+
+
+  /* Back top
+  -----------------------------------------------*/
+  
+  $(window).scroll(function() {
+        if ($(this).scrollTop() > 200) {
+        $('.go-top').fadeIn(200);
+        } else {
+          $('.go-top').fadeOut(200);
+        }
+        });   
+        // Animate the scroll to top
+      $('.go-top').click(function(event) {
+        event.preventDefault();
+      $('html, body').animate({scrollTop: 0}, 300);
+      })
+
+     
+
+      document.addEventListener('DOMContentLoaded', function() {
+        const nameElement = document.getElementById('nameElement'); // Ensure this ID matches your HTML element
+        const nameText = nameElement.textContent;
+        nameElement.textContent = ''; // Clear the text initially
+      
+        function typeLetter(index) {
+          if (index <= nameText.length) {
+            // Display text up to the current index
+            nameElement.innerHTML = nameText.substr(0, index);
+      
+            // Add a cursor-like element after the current letter
+            if (index < nameText.length) {
+              nameElement.innerHTML += '<span class="current-letter">|</span>';
+            }
+      
+            setTimeout(() => {
+              // Move to the next letter by increasing the index
+              if (index < nameText.length) {
+                nameElement.innerHTML = nameText.substr(0, index + 1);
+              }
+              typeLetter(index + 1);
+            }, 250); // Adjust the timing as needed
+          }
+        }
+      
+        typeLetter(0); // Start typing from the first letter
+      });
+      
